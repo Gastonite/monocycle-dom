f02dd78402303505dd91e55031a553e8abc83450
Clément Pasquier <clement@n00sphere>
2018-11-22 20:16:21 +0100

    View: "from" now expects (and coerces) a stream

--- View/View.js
+++ View/View.js
@@ -1,47 +1,37 @@
 const $ = require('xstream').default
 const pipe = require('ramda/src/pipe')
 const isFunction = require('ramda-adjunct/lib/isFunction').default
-const noop = require('ramda-adjunct/lib/noop').default
 const isNonEmptyString = require('ramda-adjunct/lib/isNonEmptyString').default
 const unless = require('ramda/src/unless')
+const both = require('ramda/src/both')
 const map = require('ramda/src/map')
 const isEmpty = require('ramda/src/isEmpty')
 const always = require('ramda/src/always')
 const { coerce } = require('monocycle/utilities/coerce')
-// const { pipe } = require('monocycle/utilities/pipe')
-const log = require('monocycle/utilities/log').Log('View')
-const { WithListener } = require('monocycle/components/Listener')
-const { ensurePlainObj } = require('monocycle/utilities/ensurePlainObj')
-const { EmptyObject } = require('monocycle/utilities/empty')
 const { Factory } = require('monocycle/utilities/factory')
-// const { pipe } = require('monocycle/utilities/pipe')
 const lensProp = require('ramda/src/lensProp')
 const over = require('ramda/src/over')
 const test = require('ramda/src/test')
-const defaultTo = require('ramda/src/defaultTo')
+const propEq = require('ramda/src/propEq')
+const dissoc = require('ramda/src/dissoc')
 const when = require('ramda/src/when')
-const startsWith = require('ramda/src/startsWith')
-const keys = require('ramda/src/keys')
+const propSatisfies = require('ramda/src/propSatisfies')
 const concat = require('ramda/src/concat')
 const { h } = require('snabbdom/h')
 const mergeDeepRight = require('ramda/src/mergeDeepRight')
 const { WithDynamic } = require('monocycle/components/Dynamic')
+const isFunctor = require('@f/is-functor')
 
 const makeView = pipe(
-  // log.partial('makeView()'),
   coerce,
   over(lensProp('sel'), pipe(
     unless(isNonEmptyString, always('.')),
     when(test(/^[.#]+/), concat('div')),
-    // log.partial('sel')
   )),
-
-  // x => x/*?keys($)*/,
   over(lensProp('has'), pipe(
     when(isEmpty, always('')),
   )),
   ({ has, sel, Component, ...options }) => {
-    // log('options', options)
 
     return Component({
       View: h.bind(void 0, sel, options),
@@ -51,34 +41,27 @@ const makeView = pipe(
 )
 
 const WithView = pipe(
-  // log.partial('WithView()'),
-
   over(lensProp('from'), unless(isFunction, always(void 0))),
-  // over(lensProp('Component'), unless(isFunction, (x) => {
-  //   console.log('erf')
+  ({ from, ...options }) => {
 
-  //   return makeComponent()
-  // })),
-  // x => x/*?Object.keys($)*/,
-  ({ from, Component, ...options }) => {
-
-    const defaultOptions = { ...options, Component }
+    const { Component } = options
 
     if (from)
       return WithDynamic(pipe(
         from,
-        coerce,
-        mergeDeepRight(defaultOptions),
-        // x => {
-
-        //   x.class//?
-        //   return x//? Object.keys($)
-        // },
-        makeView,
-        $.of
+        unless(both(
+          isFunctor,
+          propSatisfies(isFunction, 'subscribe'),
+        ), $.of),
+        map(pipe(
+          coerce,
+          when(propEq('has', []), dissoc('has')),
+          mergeDeepRight(options),
+          makeView
+        ))
       ))
 
-    const View = makeView(defaultOptions)
+    const View = makeView(options)
 
     return component => {
 
--- View/View.spec.js
+++ View/View.spec.js
@@ -1,42 +1,20 @@
 const test = require('ava')
-const { Stream: $ } = require('xstream')
+const { Stream: Observable } = require('xstream')
 const { WithView } = require('./View')
-const jsc = require('jsverify')
-const { diagramArbitrary, withTime } = require('cyclejs-test-helpers')
 const pipe = require('ramda/src/pipe')
 const keys = require('ramda/src/keys')
 const { makeComponent: _makeComponent } = require('monocycle/component')
 const { withViewCombiner } = require('../utilities/withViewCombiner')
 const isFunction = require('ramda-adjunct/lib/isFunction').default
-const ensureArray = require('ramda-adjunct/lib/ensureArray').default
 const { withDefaultView } = require('../utilities/withDefaultView')
 const { div } = require('@cycle/dom')
-const modules = require('snabbdom-to-html/modules')//? keys($)
+const modules = require('snabbdom-to-html/modules')
 const renderVnode = require('snabbdom-to-html/init')([
   modules.class,
   modules.props,
   modules.attributes,
   modules.style
 ])
-
-test('foo', t => {
-  t.pass();
-})
-
-test('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
-})
-
-test('dd', t => {
-  t.plan(3);
-  return $.of(1, 2, 3, 4, 1, 6)
-    .filter(n => {
-      // only even numbers
-      return n % 2 === 0;
-    })
-    .map(() => t.pass());
-});
 
 const makeComponent = pipe(
   withDefaultView,
@@ -48,12 +26,16 @@ const Component = makeComponent()
 const viewMacro = (t, getSpec) => {
 
 
-  const { input, expected, from } = getSpec()
+  const { input, expected, previousComponent } = getSpec()
 
   t.plan(2)
 
-  const component = WithView(input).apply(void 0, isFunction(from) ? [from] : [])
-
+  const component = WithView(input)
+    .apply(void 0,
+      isFunction(previousComponent)
+        ? [previousComponent]
+        : []
+    )
 
   const sinks = component({ DOM: void 0 })
 
@@ -63,7 +45,6 @@ const viewMacro = (t, getSpec) => {
     .map(renderVnode)
     .map(actual => t.is(actual, expected))
 }
-
 
 test(`creates a view that 'has' a string`, viewMacro, () => ({
   input: {
@@ -85,8 +66,8 @@ test(`creates a view from another component`, viewMacro, () => ({
     }
   },
   expected: '<div class="zo another" style="color: black">Hi world!</div>',
-  from: sources => ({
-    DOM: $.of(div('.zo', {
+  previousComponent: sources => ({
+    DOM: Observable.of(div('.zo', {
       style: {
         color: 'black'
       }
@@ -109,18 +90,11 @@ test(`creates a view that 'has' a component`, viewMacro, () => ({
   expected: '<div id="zo" class="bu"><div class="ga">meu</div></div>'
 }))
 
-test(`creates a dynamic view`, viewMacro, () => ({
+test(`creates a dynamic view from a plain object`, viewMacro, () => ({
   input: {
     Component,
-    class: {
-      ga: false,
-      zo: true
-    },
+    has: 'meu',
     from: (component, sources) => ({
-      class: {
-        bu: true,
-        ga: true,
-      },
       has: [
         'Hello ',
         component,
@@ -128,6 +102,43 @@ test(`creates a dynamic view`, viewMacro, () => ({
       ]
     })
   },
-  expected: '<div class="ga zo bu">Hello world !</div>',
-  from: sources => ({ DOM: $.of('world') })
+  expected: '<div>Hello world !</div>',
+  previousComponent: sources => ({ DOM: Observable.of('world') })
+}))
+
+test(`creates a dynamic view from an observable`, viewMacro, () => ({
+  input: {
+    Component,
+    has: 'meu',
+    from: (component, sources) => Observable.of({
+      has: [
+        'Hello ',
+        component,
+        { text: ' !' },
+      ]
+    })
+  },
+  expected: '<div>Hello world !</div>',
+  previousComponent: sources => ({ DOM: Observable.of('world') })
+}))
+
+
+test(`creates a view with dynamic classes`, viewMacro, () => ({
+  input: {
+    Component,
+    class: {
+      ga: false,
+      zo: true
+    },
+    has: 'meu',
+    from: (component, sources) => Observable.of({
+      class: {
+        bu: true,
+        ga: true,
+      },
+      has: []
+    })
+  },
+  expected: '<div class="ga zo bu">meu</div>',
+  previousComponent: sources => ({ DOM: Observable.of('world') })
 }))
